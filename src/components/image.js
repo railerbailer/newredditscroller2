9c914dc5f78c139d4451e4957135a5f8a90c2d31
Rasmus <rasmus.arvidsson1@gmail.com>
2019-04-05 07:26:34 +0200

    cleaned up image

--- src/components/image.js
+++ src/components/image.js
@@ -1,23 +1,15 @@
 import React, { Component } from "react";
 
 class Image extends Component {
-  state = {
-    loaded: false
-  };
-
   render() {
-    imageError=false
     const { className, src, onClick } = this.props;
-    const { loaded } = this.state;
 
     return (
       <img
         alt="Image could not be loaded"
         className={className}
-        ref={img => this.img = img}
+        ref={img => (this.img = img)}
         onClick={onClick}
-   
-
         src={src}
       />
     );

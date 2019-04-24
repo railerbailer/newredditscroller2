3c548ae8bb2ad913efdda141f9bab6c16a6b0e6d
railerbailer <rasmus.arvidsson1@gmail.com>
2019-04-24 22:29:04 +0200

    changed names of lazyload

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import trackerHoc from "./components/trackerHoc";
 import { carPath } from "./utils/carPath";
 
 const Scroller = lazy(() => import("./components/scroller"));
-const Collections = lazy(() => import("./components/collectionsScroller"));
+const CollectionsScroller = lazy(() => import("./components/collectionsScroller"));
 const ChooseCategory = lazy(() => import("./components/chooseCategory"));
-const userCollectionCards = lazy(() => import("./components/userCollectionCards"));
+const UserCollectionCards = lazy(() => import("./components/userCollectionCards"));
 class App extends Component {
   render() {
     return (
@@ -22,10 +22,9 @@ class App extends Component {
             </div>
           }
         >
-          <Route path="/collections" exact component={trackerHoc(userCollectionCards)} />
-          <Route path="/collections/:collection" exact component={trackerHoc(Collections)} />
+          <Route path="/collections" exact component={trackerHoc(UserCollectionCards)} />
+          <Route path="/collections/:collection" exact component={trackerHoc(CollectionsScroller)} />
           <Route path="/" exact component={trackerHoc(ChooseCategory)} />
-          <Route path="/:whatever" exact component={trackerHoc(ChooseCategory)} />
           <Route path="/subreddits/:subreddit" exact component={trackerHoc(Scroller)} />
         </Suspense>
       </BrowserRouter>
